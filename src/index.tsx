0a051c010bca0bc06d6bf07c364a81a4c96b6a85
flaxogic <flaxogic2025@outlook.com>
2025-09-25 20:50:51 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import './index.css';
 
 // PWA Service Worker Registration
 if ('serviceWorker' in navigator) {
