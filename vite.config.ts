0d3fb19651c0e7d2ad8626b2dd12ab56e9b9af1c
flaxogic <flaxogic2025@outlook.com>
2025-09-25 19:17:49 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // THIS IS THE FIX 
+      // Add this line
       base: '/nexanotes/', 
 
       plugins: [react()],
